7d9fa2fd2828d32b5709678b378d520833c50beb
Landisoa <landisoa@gmail.com>
2019-01-24 14:31:42 +0100

    updated
    Signed-off-by: Landisoa <landisoa@gmail.com>

--- Numbers/src/devops/numbers/NumberWordsApplication.java
+++ Numbers/src/devops/numbers/NumberWordsApplication.java
@@ -4,29 +4,92 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+	
 public final class NumberWordsApplication {
-	private final NumberWords numberWords ;
-	private final BufferedReader reader ;
 	
-	public NumberWordsApplication() {
-		numberWords = new NumberWords() ;
-		reader = new BufferedReader( new InputStreamReader( System.in ) ) ;
-	}
-	
-	public void execute() {
-		while( true ) {
-			try {
-				System.out.print( "Enter number (0 to exit): " ) ;
-				String value = reader.readLine() ;
-				int number = Integer.parseInt( value ) ;
-			} catch ( NumberFormatException | IOException e ) {
-				System.out.println( "Invalid number" ) ;
-			}
-		}
-	}
+	 
+		    
+		    private static final String[] tens = {
+		        "",
+		        " ten",
+		        " twenty",
+		        " thirty",
+		        " forty",
+		        " fifty",
+		        " sixty",
+		        " seventy",
+		        " eighty",
+		        " ninety"
+		    };
+		    
+		    private static final String[] nums = {
+		        "",
+		        " one",
+		        " two",
+		        " three",
+		        " four",
+		        " five",
+		        " six",
+		        " seven",
+		        " eight",
+		        " nine",
+		        " ten",
+		        " eleven",
+		        " twelve",
+		        " thirteen",
+		        " fourteen",
+		        " fifteen",
+		        " sixteen",
+		        " seventeen",
+		        " eighteen",
+		        " nineteen"
+		    };
+		    
+		    private String convertLessThanOneThousand(int number) {
+		        String current;
+		        
+		        if (number % 100 < 20){
+		            current = nums[number % 100];
+		            number /= 100;
+		        }
+		        else {
+		            current = nums[number % 10];
+		            number /= 10;
+		            
+		            current = tens[number % 10] + current;
+		            number /= 10;
+		        }
+		        if (number == 0) return current;
+		        return nums[number] + " hundred" + current;
+		    }
+		    
+		    public String convert(int number) {
 
-	public static void main(String[] args) {
-		new NumberWordsApplication().execute() ;
-	}
+		        if (number == 0) { return "zero"; }
+		        
+		        String prefix = "";
+		        
+		        if (number < 0) {
+		            number = -number;
+		            prefix = "negative";
+		        }
+		        
+		        String current = "";
+		        int place = 0;
+		        
+		        do {
+		            int n = number % 1000;
+		            if (n != 0){
+		                String s = convertLessThanOneThousand(n);
+		                current = s + current;
+		            }
+		            place++;
+		            number /= 1000;
+		        } while (number > 0);		        
+		        return (prefix + current).trim();
+       	
+		        
+		    }
+		    
 
 }
--- Numbers/test/devops/numbers/NumberWordsTests.java
+++ Numbers/test/devops/numbers/NumberWordsTests.java
@@ -1,20 +1,37 @@
 package devops.numbers;
 
 import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 import devops.numbers.NumberWords;
 
 public class NumberWordsTests {
 	private NumberWords numberWords ;
 	
-	@Before
-	public void setup() {
-		numberWords = new NumberWords() ;
-	}
-
 	@Test
-	public void numberOutOfRangeReturnsError() {
-		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( -1 ) ) ;
+		public void testonedigit() {
+		
+		NumberWordsApplication onedigit = new NumberWordsApplication();
+		Assert.assertEquals(onedigit.convert(0).trim(), "zero");
+
+	}
+	
+	@Test
+	public void testtwodigits() {
+		
+		NumberWordsApplication twodigits = new NumberWordsApplication();
+		Assert.assertEquals(twodigits.convert(10).trim(), "ten");
+
+	}
+	
+	@Test
+	public void testthreedigit() {
+		
+		NumberWordsApplication threedigits = new NumberWordsApplication();
+		Assert.assertEquals(threedigits.convert(123).trim(), "one hundred twenty three");
+
 	}
 
 }
+
